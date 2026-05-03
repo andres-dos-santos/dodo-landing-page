b9bd5a68bd43aa44155e3a73c11f435de07524f2
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2026-05-02 23:17:06 -0300

    feat: getting started

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="mx-auto h-screen max-w-5xl flex items-center justify-center">
+      <h3 className="text-sm font-medium font-sans">Em breve Dodo.app</h3>
+    </div>
+  )
+}
