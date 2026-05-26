7502c22f2a60654932cdb08712c19329fb77e3fe
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2026-05-25 22:00:13 -0300

    feat: retrieve temporary token

--- app/(mobile)/subscription/page.tsx
+++ app/(mobile)/subscription/page.tsx
@@ -1,7 +1,13 @@
-export default function SubscriptionPage() {
+export default async function SubscriptionPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+  const { token } = await searchParams // token com tempo de expiração curto
+
   return (
     <div>
-      <h1>Aqui conecta com o Stripe por exemplo</h1>
+      <h1>Aqui conecta com o Stripe por exemplo {token}</h1>
     </div>
   )
 }
